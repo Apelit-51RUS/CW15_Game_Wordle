73cf5c1ea72c426e26c38599bf5e1e15b95148fd
Apelit-51RUS <apelit1983@gmail.com>
2024-12-24 23:38:15 +0200

    Добавлены основные файлы проекта

---
+++ script.js
@@ -0,0 +1,24 @@
+const words = ["input", "array", "reduce", "class", "script"];
+
+const guess_block = document.querySelector('.guess_block');
+const guess_word = document.querySelector('#guess_word');
+const trails = document.querySelector('.trails');
+
+
+function getRundomIndex (length) {
+    return Math.trunc(Math.random() * length);
+
+}
+
+let word = words[(getRundomIndex(words.length))];
+console.log(typeof indexWord);
+let trail_num = indexWord.length + 1;
+
+function startGame () {
+    let guess_field = document.createElement('div');
+    guess_field.className = "guess_field";
+    guess_field.innerHTML = word.split('').reduce('');
+
+
+
+}
