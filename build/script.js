a72a1184953f6e3873483cc68dc8f48c7e48ed72
Apelit-51RUS <apelit1983@gmail.com>
2024-12-25 19:47:58 +0200

    Добавлен методы

--- build/script.js
+++ build/script.js
@@ -4,15 +4,13 @@ const guess_block = document.querySelector('.guess_block');
 const guess_word = document.querySelector('#guess_word');
 const trails = document.querySelector('.trails');
 
-
 function getRundomIndex (length) {
     return Math.trunc(Math.random() * length);
-
 }
 
 let word = words[(getRundomIndex(words.length))];
-console.log(typeof indexWord);
-let trail_num = indexWord.length + 1;
+
+let trail_num = word.length + 1;
 
 function startGame () {
     let guess_field = document.createElement('div');
@@ -23,6 +21,27 @@ function startGame () {
     }, "");
     guess_block.append(guess_field);
     guess_word.onkeyup = handleTrail;
+
+
+    /** @param{Event}e */
+    function handleTrail (e) {
+        e.preventDefault();
+        if (e.target.value.length !== word.length) {
+            alert("Wrong length! try again!");
+            e.target.value = "";
+            return;
+        }
+        trail_num--;
+        trails.textContent = 'Trails left: ' + trail_num;
+        let letters = document.querySelectorAll('.letter');
+        for (let i = 0; i < letters.length; i++) {
+            letters[i].textContent=e.target.value.charAt(i);
+            
+        }
+
+
+
+    }
 
 }
 startGame();
